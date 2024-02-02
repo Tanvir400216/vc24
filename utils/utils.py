62c439b159182c737b859bc3b358b9cf3ac17ae2
Tanvir055 <90306629+Tanvir055@users.noreply.github.com>
2024-02-02 22:10:27 +0600

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -254,7 +254,7 @@ async def skip():
 
 
 async def check_vc():
-    a = await bot.send(GetFullChannel(channel=(await bot.resolve_peer(Config.CHAT))))
+    a = await bot.invoke(GetFullChannel(channel=(await bot.resolve_peer(Config.CHAT))))
     if a.full_chat.call is None:
         try:
             LOGGER.info("No active calls found, creating new")
